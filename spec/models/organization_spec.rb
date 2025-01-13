34a462768f29be4aa8a4b9b32654cb2e84281c5f
Ivan Young <ivanyoung10@gmail.com>
2025-01-13 15:20:51 -0800

    Added organization_spec test

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
+  it "exists" do
+    Organization.new
+  end
+
+  it "has a name" do
+    organization = Organization.new
+    expect(organization).to respond_to(:name)
+  end
 
 end
