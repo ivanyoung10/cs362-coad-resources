173cdf7fb5b747aecf71d539bf06cf103bb59208
Ivan Young <ivanyoung10@gmail.com>
2025-01-13 15:24:28 -0800

    Finished initial tests

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
+  it "exists" do
+    ResourceCategory.new
+  end
 
+  it "has a name" do
+    resource_category = ResourceCategory.new
+    expect(resource_category).to respond_to(:name)
+  end
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
+  it "exists" do
+    Ticket.new
+  end
 
+  it "has a name" do
+    ticket = Ticket.new
+    expect(ticket).to respond_to(:name)
+  end
 end
