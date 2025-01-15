0bb8eee743cf77760c0baac9497e942bd68f7f5c
Ivan Young <ivanyoung10@gmail.com>
2025-01-15 15:02:47 -0800

    Finished ticket_spec.rb attribute test

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,12 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
+
+  let (:tick) {Ticket.new}
+
   it "exists" do
     Ticket.new
   end
 
   it "has a name" do
-    ticket = Ticket.new
-    expect(ticket).to respond_to(:name)
+    expect(tick).to respond_to(:name)
   end
+
+  it "has a description" do
+    expect(tick).to respond_to(:description)
+  end
+
+  it "has a phone" do
+    expect(tick).to respond_to(:phone)
+  end
+
+  it "has a closed" do
+    expect(tick).to respond_to(:closed)
+  end
+
+  it "has a closed at" do
+    expect(tick).to respond_to(:closed_at)
+  end
+
+  it "has a name" do
+    expect(tick).to respond_to(:name)
+  end
+
+
 end
