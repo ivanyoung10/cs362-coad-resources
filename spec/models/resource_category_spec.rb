1f4825207d4a1d3fe3c489f9e38be219e4d3a58f
Ivan Young <ivanyoung10@gmail.com>
2025-01-15 15:24:46 -0800

    Added resource_category association tests

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -15,4 +15,13 @@ RSpec.describe ResourceCategory, type: :model do
   it "has a active" do
     expect(resCat).to respond_to(:active)
   end
+
+  it "has and belongs to many organizations" do
+    should have_and_belong_to_many(:organizations)
+  end
+
+  it "has many tickets" do
+    should have_many(:tickets)
+  end
+  
 end
