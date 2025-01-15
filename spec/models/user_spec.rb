9f4a07deca1451d674da53ccc3a7d91a0f89f4be
Ivan Young <ivanyoung10@gmail.com>
2025-01-15 15:10:27 -0800

    Finished User attribute tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  let (:usr) {User.new}
+
   it "exists" do
     User.new
+  end
+
+  it "has a email" do
+    expect(usr).to respond_to(:email)
+  end
+
+  it "has a encrypted password" do
+    expect(usr).to respond_to(:encrypted_password)
+  end
+
+  it "has a reset password token" do
+    expect(usr).to respond_to(:reset_password_token)
+  end
+
+  it "has a reset password sent at" do
+    expect(usr).to respond_to(:reset_password_sent_at)
+  end
+
+  it "has a confirmation token" do
+    expect(usr).to respond_to(:confirmation_token)
+  end
+
+  it "has a confirmation at" do
+    expect(usr).to respond_to(:confirmed_at)
+  end
+
+  it "has a confirmation sent at" do
+    expect(usr).to respond_to(:confirmation_sent_at)
+  end
+
+  it "has a unconfirmed email" do
+    expect(usr).to respond_to(:unconfirmed_email)
+  end
+
+  it "role" do
+    expect(usr).to respond_to(:role)
   end
 
 end
