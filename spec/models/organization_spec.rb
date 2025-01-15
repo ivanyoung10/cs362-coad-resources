3c6944a10bb07024bddb41e67a974387b03dc6ea
Ivan Young <ivanyoung10@gmail.com>
2025-01-15 14:39:27 -0800

    Finished organization attribute tests

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,13 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
+
+  let (:org) { Organization.new}
+
   it "exists" do
     Organization.new
   end
 
   it "has a name" do
-    organization = Organization.new
-    expect(organization).to respond_to(:name)
+    expect(org).to respond_to(:name)
   end
+
+  it "has a status" do
+    expect(org).to respond_to(:status)
+  end
+
+  it "has a phone" do
+    expect(org).to respond_to(:phone)
+  end
+
+  it "has a email" do
+    expect(org).to respond_to(:email)
+  end
+
+  it "has a description" do
+    expect(org).to respond_to(:description)
+  end
+
+  it "has a rejection_reason" do
+    expect(org).to respond_to(:rejection_reason)
+  end
+
+  it "has a phone" do
+    expect(org).to respond_to(:phone)
+  end
+
+  it "has a libality insurance" do
+    expect(org).to respond_to(:liability_insurance)
+  end
+
+  it "has a primary name" do
+    expect(org).to respond_to(:primary_name)
+  end
+
+  it "has a secondary name" do
+    expect(org).to respond_to(:secondary_name)
+  end
+
+  it "has a secondary phone" do
+    expect(org).to respond_to(:secondary_phone)
+  end
+
+  it "has a title" do
+    expect(org).to respond_to(:title)
+  end
+
+  it "has a transportation" do
+    expect(org).to respond_to(:transportation)
+  end
+
+  
 
 end
