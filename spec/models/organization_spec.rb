9d3c80ccbb8aaa77558a1f921c64151ef9a5c26f
Ivan Young <ivanyoung10@gmail.com>
2025-01-15 15:21:30 -0800

    Added association tests

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -60,6 +60,17 @@ RSpec.describe Organization, type: :model do
     expect(org).to respond_to(:transportation)
   end
 
+  it "has many users" do
+    should have_many(:users)
+  end
+
+  it "has many tickets" do
+    should have_many(:tickets)
+  end
+
+  it "has many and belongs to many resource categories" do
+    should have_and_belong_to_many(:resource_categories)
+  end
   
 
 end
