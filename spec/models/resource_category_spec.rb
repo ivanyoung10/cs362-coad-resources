6f577e87151c0151f34631cf565f4a80e49f49e5
Ivan Young <ivanyoung10@gmail.com>
2025-01-15 14:57:19 -0800

    Finished resource category attribute tests

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
+
+  let (:resCat) {ResourceCategory.new}
+
   it "exists" do
     ResourceCategory.new
   end
 
   it "has a name" do
-    resource_category = ResourceCategory.new
-    expect(resource_category).to respond_to(:name)
+    expect(resCat).to respond_to(:name)
+  end
+
+  it "has a active" do
+    expect(resCat).to respond_to(:active)
   end
 end
