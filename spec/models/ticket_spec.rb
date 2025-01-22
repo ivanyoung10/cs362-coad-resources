b1b5ba446310ff9a46ce9d2ad6d1080e52d1058d
Ivan Young <ivanyoung10@gmail.com>
2025-01-22 14:30:08 -0800

    Started member function and validation tests

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,48 +2,67 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
-  let (:tick) {Ticket.new}
+  let (:ticket) { Ticket.new(id: 123) }
 
-  it "exists" do
-    Ticket.new
+  describe "attribute tests" do
+    it "exists" do
+      Ticket.new
+    end
+
+    it "has a name" do
+      expect(ticket).to respond_to(:name)
+    end
+
+    it "has a description" do
+      expect(ticket).to respond_to(:description)
+    end
+
+    it "has a phone" do
+      expect(ticket).to respond_to(:phone)
+    end
+
+    it "has a closed" do
+      expect(ticket).to respond_to(:closed)
+    end
+
+    it "has a closed at" do
+      expect(ticket).to respond_to(:closed_at)
+    end
+
+    it "has a name" do
+      expect(ticket).to respond_to(:name)
+    end
+
+    it "belongs to a region" do
+      should belong_to(:region)
+    end
+
+    it "belongs to a resource category" do 
+      should belong_to(:resource_category)
+    end
+
+    it "belongs to a organization" do 
+      should belong_to(:organization).optional
+    end
   end
 
-  it "has a name" do
-    expect(tick).to respond_to(:name)
+  describe "validation tests" do 
+    it "validates presence of name" do
+      expect(ticket).to validate_presence_of(:name)
+    end
+
+    it "validates name length" do
+      expect(ticket).to validate_length_of(:name).is_at_most(255)
+      expect(ticket).to validate_length_of(:name).is_at_least(1)
+    end
+
   end
 
-  it "has a description" do
-    expect(tick).to respond_to(:description)
+  describe "member function tests" do
+    it "converts to string" do
+      expect(ticket.to_s).to eq "Ticket 123"
+    end
   end
-
-  it "has a phone" do
-    expect(tick).to respond_to(:phone)
-  end
-
-  it "has a closed" do
-    expect(tick).to respond_to(:closed)
-  end
-
-  it "has a closed at" do
-    expect(tick).to respond_to(:closed_at)
-  end
-
-  it "has a name" do
-    expect(tick).to respond_to(:name)
-  end
-
-  it "belongs to a region" do
-    should belong_to(:region)
-  end
-
-  it "belongs to a resource category" do 
-    should belong_to(:resource_category)
-  end
-
-  it "belongs to a organization" do 
-    should belong_to(:organization).optional
-  end
-
 
 
 end
