77d25212ba1b0083ce61695280ac7dd4f08ab1c9
Rowan Dey <rowandey0@oregonstate.edu>
2025-02-15 14:40:13 -0800

    Updated tickets to use res_cat instead of org

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -1,11 +1,11 @@
 #Ivan
- FactoryBot.define do
+FactoryBot.define do
   factory :ticket do
     sequence(:name)  {|n| "ticket#{n}"}
     sequence(:region_id)  {|n| "region#{n}"}
-    sequence(:organization_id)  {|n| "organization#{n}"}
+    sequence(:resource_category_id)  {|n| "resource_category#{n}"}
 
-    phone { '+1-111-111-1111' }
+    phone { "+1-555-555-1212" }
   end
 
- end
+end
