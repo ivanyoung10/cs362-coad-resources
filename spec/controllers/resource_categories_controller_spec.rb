08839019e87c34469f8b2a5ac4f48f7209f33504
Ivan Young <ivanyoung10@gmail.com>
2025-02-17 12:38:07 -0800

    Started controller tests

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
 
-RSpec.describe ResourceCategoriesController, type: :controller do
+#Ivan
 
+#  PATCH    /resource_categories/:id/activate(.:format)               resource_categories#activate
+#  PATCH    /resource_categories/:id/deactivate(.:format)             resource_categories#deactivate
+#  GET      /resource_categories(.:format)                            resource_categories#index --DONE
+#  POST     /resource_categories(.:format)                            resource_categories#create --DONE
+#  GET      /resource_categories/new(.:format)                        resource_categories#new 
+#  GET      /resource_categories/:id/edit(.:format)                   resource_categories#edit
+#  GET      /resource_categories/:id(.:format)                        resource_categories#show
+#  PATCH    /resource_categories/:id(.:format)                        resource_categories#update
+#  PUT      /resource_categories/:id(.:format)                        resource_categories#update
+#  DELETE / resource_categories/:id(.:format)                        resource_categories#destroy
+
+RSpec.describe ResourceCategoriesController, type: :controller do
+  describe 'as a logged out user' do
+    let(:resource_category) { FactoryBot.create(:resource_category) }
+    let(:user) { create(:user) }
+
+    it { expect(get(:index)).to redirect_to new_user_session_path }
+    it { expect(get(:new)).to redirect_to new_user_session_path }
+    it {
+      post(:create, params: { resource_category: FactoryBot.attributes_for(:resource_category) })
+      expect(response).to redirect_to new_user_session_path
+    }
+  end
+
+  describe 'as a logged in user' do
+    let(:resource_category) { FactoryBot.create(:resource_category) }
+    let(:user) { create(:user) }
+    before(:each) { sign_in user }
+
+    it { expect(get(:index)).to redirect_to dashboard_path }
+
+    it { expect(get(:new)).to redirect_to dashboard_path }
+    
+    it {
+      post(:create, params: { resource_category: FactoryBot.attributes_for(:resource_category) })
+      expect(response).to redirect_to dashboard_path
+    }
+  end
+
+  describe 'as an admin' do
+    let(:resource_category) { FactoryBot.create(:resource_category) }
+    let(:user) { create(:user, :admin) }
+    before(:each) { sign_in user }
+
+    it { expect(get(:index)).to be_successful }
+    it { expect(get(:new)).to be_successful }
+    it {
+      post(:create, params: { resource_category: FactoryBot.attributes_for(:resource_category) })
+      expect(response).to redirect_to resource_categories_path
+    }
+  end
 end
--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,5 +1,70 @@
 require 'rails_helper'
+# POST   /tickets(.:format)                                                                       tickets#create  --DONE
+# GET    /tickets/new(.:format)                                                                   tickets#new --DONE
+# GET    /tickets/:id(.:format)                                                                   tickets#show
+# GET    /ticket_submitted(.:format)                                                              static_pages#ticket_submitted
+# GET    /organization_expectations(.:format)                                                     static_pages#organization_expectations
+# POST   /tickets/:id/capture(.:format)                                                           tickets#capture
+# POST   /tickets/:id/release(.:format)                                                           tickets#release
+# PATCH  /tickets/:id/close(.:format)                                                             tickets#close
 
 RSpec.describe TicketsController, type: :controller do
+  describe 'as a logged out user' do
+    resource_category = FactoryBot.create(:resource_category)
+    region = FactoryBot.create(:region)
+    let (:ticket) { FactoryBot.create(:tic)}
+    let(:user) { create(:user) }
 
+    it { expect(get(:new)).to be_successful }
+
+    it {
+      get :show, params: { id: ticket.id }
+      expect(response).to redirect_to new_user_session_path
+    }
+
+    it {
+      post(:create, params: { ticket: FactoryBot.attributes_for(:ticket) })
+      expect(response).to be_successful
+    }
+  end
+
+  describe 'as a logged in user' do
+    let(:ticket) { FactoryBot.create(:ticket) }
+    let(:resource_category) { FactoryBot.create(:ticket) }
+    let(:region) { FactoryBot.create(:region) }
+    let(:user) { create(:user) }
+    before(:each) { sign_in user }
+
+    it { expect(get(:new)).to be_successful }
+
+    it {
+      get :show, params: { id: ticket.id }
+      expect(response).to redirect_to dashboard_path
+    }
+
+    it {
+      post(:create, params: { ticket: FactoryBot.attributes_for(:ticket) })
+      expect(response).to be_successful
+    }
+  end
+
+  describe 'as an admin' do
+    let(:ticket) { FactoryBot.create(:ticket) }
+    let(:resource_category) { FactoryBot.create(:ticket) }
+    let(:region) { FactoryBot.create(:region) }
+    let(:user) { create(:user, :admin) }
+    before(:each) { sign_in user }
+
+    it { expect(get(:new)).to be_successful }
+
+    it {
+      post(:create, params: { ticket: FactoryBot.attributes_for(:ticket) })
+      expect(response).to be_successful
+    }
+
+    it {
+      get :show, params: { id: ticket.id }
+      expect(response).to be_successful
+    }
+  end
 end
